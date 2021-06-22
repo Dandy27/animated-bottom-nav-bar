20302198e42b361ddaf2ffebb15fd668ca0eda50
dandy <danielsbarbosa27@gmail.com>
2021-06-22 04:55:18 -0300

    Home

---
+++ appanimation/android/app/src/main/kotlin/com/example/animationbottom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animationbottom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ appanimation/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: buildBody(),
+    );
+  }
+
+  Widget buildBody() => Center(
+      child: Text(
+        'Animated Bottom \nNavigation',
+        style: TextStyle(fontSize: 30, color: Colors.black87,),
+        textAlign: TextAlign.center
+      ),
+    );
+}
